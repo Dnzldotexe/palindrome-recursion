17635272e28ef37bdedcaba8b1280a8902dfa4dc
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-05-27 07:26:19 +0000

    finished the code logic

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ checking if a string is palindrome or not, but this time, I'll be using recursio
 - Any longer string is a palindrome is the first and last characters match.
 - Display appropriate output to the user.
 
-### NOTE:
+### NOTES:
 - Clean the string first before applying the palindrome function.
 - The case of letters doesn't matter.
 
--- recursive_palindrome.py
+++ recursive_palindrome.py
@@ -1,3 +1,11 @@
+'''
+This python module contains three functions.
+(1) clean_string()
+(2) recursive_palindrome()
+(3) main()
+
+'''
+
 def clean_string(unclean):
     '''Removes special characters in a string
 
@@ -11,7 +19,7 @@ def clean_string(unclean):
     '''
     letters = "abcdefghijklmnopqrstuvwxyz"
 
-    clean = [letter for letter in unclean if letter.lower() in letters]
+    clean = [letter.lower() for letter in unclean if letter.lower() in letters]
 
     if not clean:
         return None
@@ -20,14 +28,44 @@ def clean_string(unclean):
 
 
 def recursive_palindrome(text):
-    pass
+    '''Determines if a string is palindrome recursively
+
+    Args: 
+        string (str) text
+
+    Returns:
+        True
+        False
+
+    '''
+    if len(text) < 2:
+        print("String is a Palindrome.")
+        return True
+
+    if text[0] != text[-1]:
+        print("String is not a Palindrome.")
+        return False
+
+    return recursive_palindrome(text[1:-1])
 
 
 def main():
-    pass
+    '''Demonstrates the recursive_palindrome() function
+    
+    Prints: 
+        string (str) text
+
+    '''
+    while True:                                             # Loops until input is empty
+        words = input("\nIs it a palindrome: ").strip()
+
+        if not words:                                       # Returns when input is empty
+            return None
+
+        words = clean_string(words)
+        result = recursive_palindrome(words)                # Stores the result of the function call
+
+        print(f"Result: {result}.")                         # Prints the result
 
 
-case = "A man, a plan, a canal, Panama."
-print(len("AmanaplanacanalPanama"))
-#print(clean_string(case))
-print(len(clean_string(case)))
+main()                                                      # Runs the function
