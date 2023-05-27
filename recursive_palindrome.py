70128163e6d322f62cf298d4a58c3f8af8889958
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-05-27 06:39:53 +0000

    finished the string cleaning function

--- recursive_palindrome.py
+++ recursive_palindrome.py
@@ -1,8 +1,33 @@
-def clean_string(s):
+def clean_string(unclean):
+    '''Removes special characters in a string
+
+    Args: 
+        string (str) text
+
+    Returns:
+        None
+        List of (list) lowercase letters
+
+    '''
+    letters = "abcdefghijklmnopqrstuvwxyz"
+
+    clean = [letter for letter in unclean if letter.lower() in letters]
+
+    if not clean:
+        return None
+
+    return clean
+
+
+def recursive_palindrome(text):
     pass
 
-def recursive_palindrome(s):
-    pass
 
 def main():
     pass
+
+
+case = "A man, a plan, a canal, Panama."
+print(len("AmanaplanacanalPanama"))
+#print(clean_string(case))
+print(len(clean_string(case)))
